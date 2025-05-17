d21d6af0e90d4aa07945e04e1bd22fc47b9976c9
mike <jfkz@users.noreply.github.com>
2025-05-17 16:26:27 +0200

    feat: analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import { Background } from './components/Background';
 import { FormContainer } from './components/FormContainer';
 import { SubscribeForm } from './components/SubscribeForm';
+import ReactGA from 'react-ga4';
+
 import './styles/animations.css';
+
+const GA_TRACKING_ID = import.meta.env.VITE_GA_TRACKING_ID || '';
+ReactGA.initialize(GA_TRACKING_ID, {
+  gtagOptions: {
+    'js': new Date(),
+    'config': GA_TRACKING_ID,
+  }
+});
+ReactGA.send('pageview');
+
 
 function App() {
   return (
